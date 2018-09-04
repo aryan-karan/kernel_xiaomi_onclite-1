015ff040270ee8e3996529a72472dee35e4a8714
Francisco Franco <franciscofranco.1990@gmail.com>
2018-09-04 02:29:30 +0000

    fork: boost the cpu to max freq when an app is launched
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Hadad <repo-sync@outlook.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -81,6 +81,7 @@
 #include <linux/kcov.h>
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
+#include <linux/cpu_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2113,6 +2114,9 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+
+	if (is_zygote_pid(current->pid))
+		do_input_boost_max();
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
